b7361c9918b69085389922e45e9f8b0d908a684c
Victor <vgarcialosada@gmail.com>
2022-09-06 09:25:39 +0200

    first commit

---
+++ src/main/java/com/example/LeaguePongApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaguePongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaguePongApplication.class, args);
+	}
+
+}
