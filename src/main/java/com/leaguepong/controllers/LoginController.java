edcdf1c15a5f8590ecf0d7357987fd58443de3e4
vgarcialosada <vgarcialosada@gmail.com>
2022-09-08 13:56:25 +0200

    login1

---
+++ src/main/java/com/leaguepong/controllers/LoginController.java
@@ -0,0 +1,10 @@
+package com.leaguepong.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+@GetMapping("/login")
+public String login() {return "index";}
+}
---
+++ src/main/java/com/services/PasswordEncrypt.java
@@ -0,0 +1,18 @@
+package com.services;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+public class PasswordEncrypt {
+	
+	
+	// ENCRIPTAR CONTRASEÑA
+	public class SecuredPasswordGenerator {
+		public String main(String pwd) {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			String encodedPassword = encoder.encode(pwd);
+			return encodedPassword;
+		}
+
+	}
+}
