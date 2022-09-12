df29c812907892a1b3186a70913e1a0c2fa899c1
vgarcialosada <vgarcialosada@gmail.com>
2022-09-12 12:39:36 +0200

    login sin pwd 2

---
+++ src/main/java/com/leaguepong/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.leaguepong.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/test")
+    public String welcome() {
+        return "index";
+    }
+}
