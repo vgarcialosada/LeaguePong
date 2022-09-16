0f47db5e01c93327add252ee99b50d16d9527cde
vgarcialosada <vgarcialosada@gmail.com>
2022-09-16 09:58:09 +0200

    axios

--- src/main/resources/static/js/create_user.js
+++ src/main/resources/static/js/create_user.js
@@ -1,6 +1,18 @@
-//mail valid
+import axios from "axios";
+
+function crear_usuario_data() {
+  fetch("http://127.0.0.1:8080/register_user")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+    //console.log(data[0]);
+    //for( i = 0; i < data.length; i++){
+      //console.log(data[i]);
+       // return data[i];
+    //} 
+}
 
 
+//comprueba pwd iguales
 function passCheck() {
   var password = document.getElementById('password');
   var vpassword = document.getElementById('confirm_password');
