00ba88e5226ce81bb61d373b93080c35de8cde9c
Dawit Elias <dawitelias733@gmail.com>
2022-09-19 10:11:27 +0200

    view console

--- src/main/resources/static/js/my_leagues.js
+++ src/main/resources/static/js/my_leagues.js
@@ -1,20 +1,17 @@
-// function mis_ligas(id) {
-//     fetch("http://127.0.0.1:8080/" + id + "/mis-ligas")
-//       .then((response) => response.json())
-//       ;
-//   }
-//   //console.log("hello");
-//   document.getElementById("my_leagues").innerHTML = mis_ligas(1);
-  const mis_ligas = async(id) => (await fetch("http://127.0.0.1:8080/" + id + "/mis-ligas")).json();
+async function mis_ligas(id) {
+  res = await fetch("http://127.0.0.1:8080/" + id + "/mis-ligas").then(
+    (response) => response.json()
+  );
+  res1 = res.map((res) => res.nombre);
+  console.log(res1);
+  document.getElementById("my_leagues").innerHTML = res1;
+}
+//console.log("hello");
+//document.getElementById("my_leagues").innerHTML = mis_ligas(1);
+//const mis_ligas = async (id) =>
+//(await fetch("http://127.0.0.1:8080/" + id + "/mis-ligas")).json();
 
-  mis_ligas(1)
-  .then((data) => console.log(data))
-  .then(data => {
-    for (i = 0; i < data.length; i++){
-      console.log(data[i].nombre);
-    };
-  });
-    
-
-    
-  
+mis_ligas(1);
+//.then(
+//(data) => (document.getElementById("my_leagues").innerHTML = data.map(nombre))
+//);
