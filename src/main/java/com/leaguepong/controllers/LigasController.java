8a6f55d2a0f68133c3c8d4db20499ac0e1f611f9
FrancOlano <olanofranco1@gmail.com>
2022-09-19 09:48:59 +0200

    todas ligas pero en las que no estes

--- src/main/java/com/leaguepong/controllers/LigasController.java
+++ src/main/java/com/leaguepong/controllers/LigasController.java
@@ -28,26 +28,32 @@ public class LigasController {
 
 	@CrossOrigin(origins="*", maxAge = 3600)
 
-	@GetMapping("/todas-ligas")
-	public List<Liga> allLeagues(@RequestParam(required = false) String name,
+	@GetMapping("{id}/todas-ligas")
+	public List<Liga> allLeagues(@PathVariable long id, @RequestParam(required = false) String name,
 			@RequestParam(required = false) String location) {
 		List<Liga> ligaArr = new ArrayList<Liga>();
 		String QUERY;
 		if (name == null && location == null) {
-			QUERY = "select * from leaguepong.ligas;";
+			QUERY = "select distinct ligas.ID_LIGA, NOMBRE, PASSWORD, REGLAS, UBICACION, NUMERO_JUGADORES\r\n"
+					+ " from leaguepong.ligas inner join usuarios_ligas on ligas.id_liga = usuarios_ligas.id_liga\r\n"
+					+ " where ID_USUARIO != "+id+" and nombre not in\r\n"
+					+ " (select nombre from ligas inner join usuarios_ligas on usuarios_ligas.id_liga = ligas.id_liga where id_usuario="+id+");";
 		} else if (name != null && location == null) {
 			QUERY = "select ligas.ID_LIGA, NOMBRE, PASSWORD, REGLAS, UBICACION, NUMERO_JUGADORES"
 					+ " from leaguepong.ligas inner join leaguepong.usuarios_ligas on ligas.ID_LIGA"
-					+ " = usuarios_ligas.ID_LIGA where NOMBRE LIKE \"%" + name + "%\";";
+					+ " = usuarios_ligas.ID_LIGA where NOMBRE LIKE \"%" + name + "%\" and ID_USUARIO != "+id+" and nombre not in\r\n"
+					+ " (select nombre from ligas inner join usuarios_ligas on usuarios_ligas.id_liga = ligas.id_liga where id_usuario="+id+");";
 		} else if (name == null && location != null) {
 			QUERY = "select ligas.ID_LIGA, NOMBRE, PASSWORD, REGLAS, UBICACION, NUMERO_JUGADORES"
 					+ " from leaguepong.ligas inner join leaguepong.usuarios_ligas on ligas.ID_LIGA"
-					+ " = usuarios_ligas.ID_LIGA where UBICACION LIKE \"%" + location + "%\";";
+					+ " = usuarios_ligas.ID_LIGA where UBICACION LIKE \"%" + location + "%\" and ID_USUARIO != "+id+" and nombre not in\r\n"
+					+ " (select nombre from ligas inner join usuarios_ligas on usuarios_ligas.id_liga = ligas.id_liga where id_usuario="+id+");";
 		} else {
 			QUERY = "select ligas.ID_LIGA, NOMBRE, PASSWORD, REGLAS, UBICACION, NUMERO_JUGADORES"
 					+ " from leaguepong.ligas inner join leaguepong.usuarios_ligas on ligas.ID_LIGA"
 					+ " = usuarios_ligas.ID_LIGA where UBICACION LIKE \"%" + location + "%\" and NOMBRE LIKE \"%" + name
-					+ "%\";";
+					+ "%\" and ID_USUARIO != "+id+" and nombre not in\r\n"
+					+ " (select nombre from ligas inner join usuarios_ligas on usuarios_ligas.id_liga = ligas.id_liga where id_usuario="+id+");";
 		}
 		List<Map<String, Object>> results = jdbcTemplate.queryForList(QUERY);
 		for (Map<String, Object> result : results) {
