a86f4a56dcd3e1a5a3716c7bf576ef9846d365e5
FrancOlano <olanofranco1@gmail.com>
2022-09-21 12:15:32 +0200

    commit legue view

--- src/main/resources/static/js/my_leagues.js
+++ src/main/resources/static/js/my_leagues.js
@@ -4,12 +4,12 @@ async function mis_ligas(id) {
   );
   for (i = 0; i < res.length; i++) {
 
-    document.getElementById("my_leagues").innerHTML += `<div class="card">
+    document.getElementById("my_leagues").innerHTML += `<a href="../html/league_view.html" ><div class="card">
                       <h2>${res[i].nombre}</h2>
                       <h4>${res[i].reglas}</h4>
                       <h4>${res[i].ubicacion}</h4>
                       <h4>${res[i].numero_jugadores}</h4>
-                    </div> <br><br>`;
+                    </div></a> <br><br>`;
 
     document.getElementsByClassName(
       "card"
