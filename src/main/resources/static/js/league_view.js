e30cd7e78154869063ee4aa77127bf7ae5db7eed
FrancOlano <olanofranco1@gmail.com>
2022-09-27 12:01:48 +0200

    comit no hay mas pertidos pendientes

--- src/main/resources/static/js/league_view.js
+++ src/main/resources/static/js/league_view.js
@@ -11,7 +11,13 @@ async function partidos_liga(id_liga) {
   res = await fetch(
     "http://127.0.0.1:8080/" + id_liga + "/get-partidos-por-jugar"
   ).then((response) => response.json());
-
+  if(res.length == 0){
+    document.getElementById(
+      "partidos_liga"
+    ).innerHTML =`<div class="card">No hay partidos pendientes</div>`
+  }
+  else{
+ 
   for (i = 0; i < res.length; i++) {
     resplayer1 = await fetch(
       "http://127.0.0.1:8080/" + res[i].id_jugador_1 + "/usuario"
@@ -29,12 +35,13 @@ async function partidos_liga(id_liga) {
       "partidos_liga"
     ).innerHTML += `<div class="card" style="color:white;">
                     	<div id="partido${idPartido}">  <h4>  
-                    ${resplayer1[0].nombre_usuario} <img src='../img/r2.png' style='width:55px; height:30px;''"> <img src='../img/vs.png' style='width:55px; height:55px;''">  <img src='../img/r1.png' style='width:55px; height:30px;''">  ${resplayer2[0].nombre_usuario}
+                    ${resplayer1[0].nombre_usuario} <img src='../img/r2.png' style='width:50px; height:50px;''"> <img src='../img/vs.png' style='width:55px; height:55px;''">  <img src='../img/r1.png' style='width:50px; height:50px;''">  ${resplayer2[0].nombre_usuario}
                       <div id="whowon${idPartido}"><br>  <button class="btn btn-secondary btn-sm" 
                        onclick="setWinner(${idPartido},'${resplayer1[0].nombre_usuario}','${resplayer2[0].nombre_usuario}'	)
                        ">Ganador</button></h4>
                     </div>`;
   }
+}
 }
 
 //define id de ganador de partido
